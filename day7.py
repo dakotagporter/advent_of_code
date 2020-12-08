b027cd884075a26950b2a7622204715ab8d0fe77
dakotagporter <dakotap3045@gmail.com>
2020-12-08 15:57:13 -0700

    push commits

--- day7.py
+++ day7.py
@@ -11,11 +11,49 @@ enforce!
 """
 
 
+def filter(raw_input):
+    values = []
+    color_tree = {}
+    for line in raw_input:
+        words = line.split(' ')
+        key = ' '.join(words[:2])
+        for i in range(0, len(words)):
+            if words[i].isnumeric():
+                sub_color = words[i+1:i+3]
+                values.append(' '.join(sub_color))
+
+        color_tree[key] = values
+        values = []
+
+    verify(color_tree)
+
+
+def verify(color_tree):
+    my_bag = 'shiny gold'
+    valid_bag = []
+
+    for key in list(color_tree.keys()):
+        print(color_tree['dull crimson'])
+        for i in range(0, len(color_tree[key])):
+            print(color_tree[key])
+            bag = color_tree[key][i]
+            if bag == 'shiny gold':
+                print('has shiny gold!')
+                valid_bag.append(key)
+            else:
+                key = bag
+                print(key)
+
+
+    #print(valid_bag)
+    print(color_tree['bright brown'])
+
+
 if __name__ == '__main__':
     raw_input = []
 
     # opens corresponding input file, strips newline, adds to input list
-    with open('day4.txt', 'r') as fh:
+    with open('day7.txt', 'r') as fh:
         for item in fh.readlines():
             raw_input.append(item.strip("\n"))
 
